b0c04ea54eaa706184d0ec6c2718d33766b8f1f5
annakrystalli <annakrystalli@googlemail.com>
2016-05-13 09:56:20 +0100

    clean up workflow

--- data_merging.R
+++ data_merging.R
@@ -1,5 +1,24 @@
-install.packages("prodlim")
+# SETUP ###############################################################
+
+
+rm(list=ls())
+
+source("~/Documents/workflows/bird_trait_networks/Setup.R")
+setwd(input.folder)
+
+# PACKAGES & FUNCTIONS ###############################################################
+
+source(paste(script.folder, "functions.R", sep = ""))
+
+
+# install.packages("prodlim")
 require(prodlim)
+
+# SETTINGS ###############################################################
+
+
+
+
 
 # Determine outliers
 
@@ -8,11 +27,22 @@ D0 <- read.csv(file = paste(input.folder, "csv/","D0.csv", sep = "")
 D1 <- read.csv(file = paste(input.folder, "csv/","D1.long.csv", sep = ""),
                fileEncoding = "mac") 
 
+### Remove duplicates from D0. 
+D0 <- D0[!duplicated(D0[,c("species", "var")]),]
+
+## Check 
+
 D <- rbind(cbind(D0[,c("species", "var", "value")], dataset = "D0"),
            cbind(D1[,c("species", "var", "value")], dataset = "D1"))
 
-D0.dup <- D0[duplicated(D0[,c("species", "var")], fromLast = F) | duplicated(D0[,c("species", "var")], fromLast = T),
-             c("species", "var", "value", "units", "ref")]
+
+
+# Deal with outliers
+outliers.done <- read.csv("~/Google Drive/bird trait networks/inputs/data/r data/outliers done.csv", stringsAsFactors=FALSE)
+
+
+
+
 
 
 dups <- D[duplicated(D[,c("species", "var")]),c("species", "var")]
@@ -26,8 +56,10 @@ D0[D0$species == dups$species & D0$var == dups$var, ]
 
 
 if(D0var != "bill.len"){
-  df <- cbind(D0.var, D1.var)
+  df <- cbind(dat$D0.value, dat$D1.value)
   outliers <- apply(df, 1, FUN = 
                       function(x){any(abs(diff(x))/x[which(x == min(x))] > 2)})
+  outlie.df <- dat[!outliers,]
+  
   outlie.df <- rbind(outlie.df, cbind(species = spp, var = D0var, df)[outliers,])
 } 
