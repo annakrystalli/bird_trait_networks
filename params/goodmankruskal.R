fd329fb557fe3e3e5f8890c47d2e630023d7490f
annakrystalli <annakrystalli@googlemail.com>
2016-09-09 15:46:23 +0100

    commit new params and data gap evaluation Rmd

---
+++ params/goodmankruskal.R
@@ -0,0 +1,11 @@
+## ---- param ----
+#an.ID <- "100spp"
+an.ID <- "all"
+min.n <- 50
+log <- T
+cutoff <- 0.35
+min.cat <- 5
+v.p <- 1
+s.p <- 1
+remove_dtips <- T
+plot.type <- "heatmap"
---
+++ params/min.n_test.R
@@ -0,0 +1,11 @@
+## ---- param ----
+#an.ID <- "100spp"
+an.ID <- "all"
+min.n <- 1
+n.spl_size <- 400
+log <- T
+cutoff <- 0.35
+min.cat <- 5
+v.p <- 0.2
+s.p <- 0.05
+remove_dtips <- T
