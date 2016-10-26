9386f2d76b9b7193aebbf3c62bd2c9ca0f0e4991
annakrystalli <annakrystalli@googlemail.com>
2016-10-26 12:17:45 +0100

    Removed macroRDM local folder link.

--- file_setup.R
+++ file_setup.R
@@ -1,8 +1,8 @@
 
-## ---- data-setup ----
+## ---- fs-data-setup ----
 output.folder <- "/Users/Anna/Google Drive/bird trait networks/outputs/"
 input.folder <- "/Users/Anna/Google Drive/bird trait networks/inputs/data/"
 
-## ---- script-setup ----
+## ---- fs-script-setup ----
 script.folder <- "~/Documents/workflows/bird_trait_networks/"
-rmacro.folder <- "~/Documents/workflows/rmacroRDM/R/"
+
