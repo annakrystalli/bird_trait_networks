ecafc4aee054e9f2956748073783677a41e56509
annakrystalli <annakrystalli@googlemail.com>
2016-12-20 13:47:13 +0200

    Move rmacro laoding to load_global.R. Tidy chunks

--- load_global.R
+++ load_global.R
@@ -6,20 +6,26 @@
 
 #' global settings
 
-## ---- load_global ----
+## ---- lg-global_options ----
 options(stringsAsFactors = F)
 
-# dependencies
+#' dependencies
+# ## ---- lg-read_pkgs ----
 source(paste(script.folder, "pkgs.R", sep = ""))
 
-# install and load dependencies through pkg "pacman"
+#' install and load dependencies through pkg "pacman"
+## ---- lg-load_pkgs ----
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(pkgs, character.only = T)
+eval(parse(text = getURL("https://raw.githubusercontent.com/annakrystalli/rmacroRDM/master/R/functions.R", ssl.verifypeer = FALSE)))
+eval(parse(text = getURL("https://raw.githubusercontent.com/annakrystalli/rmacroRDM/master/R/wideData_function.R", ssl.verifypeer = FALSE)))
 
-# load files
+#' load files
+## ---- lg-load_files ----
 source(paste(script.folder, "load_files.R", sep = ""))
 
-# load helper functions
+#' load helper functions
+## ---- lg-load_helper_functions ----
 source(paste(script.folder, "helper_functions.R", sep = ""))
 
 
