9660e45f920a4909b1a4006729bd5cd3ec80a777
annakrystalli <annakrystalli@googlemail.com>
2016-12-20 13:49:05 +0200

    add RCurl, plotrix and visNetwork

--- pkgs.R
+++ pkgs.R
@@ -3,6 +3,6 @@ pkgs <- c("dplyr", "taxize","caper", "geiger","PHYLOGR",
 "rnetcarto", "igraph", "phytools", "DT", "vegan",
 "MCMCglmm", "mgm", "devtools", "flexdashboard", "knitr", 
 "rmarkdown", "markdown", "shiny", "missForest", "networkD3", "Rphylopars",
-"GoodmanKruskal", "plotly", "reshape2")
+"GoodmanKruskal", "plotly", "reshape2", "RCurl", "plotrix", "visNetwork")
   
 
