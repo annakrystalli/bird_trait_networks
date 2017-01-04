c504532b33893931568f811995f1c36b66ee4aac
annakrystalli <annakrystalli@googlemail.com>
2017-01-04 19:48:08 +0100

    remove read.chunks() and rename chunks

--- project_ui.R
+++ project_ui.R
@@ -1,21 +1,7 @@
-# ----- req-knit ----
-require(knitr)
 
-# ----- read_chunks -----
-read_chunk(paste(script.folder, "file_setup.R", sep = ""))
-read_chunk(paste(script.folder, "pkgs.R", sep = ""))
-read_chunk(paste(script.folder, "load_dependencies.R", sep = ""))
-read_chunk(paste(script.folder, "load_global.R", sep = ""))
-read_chunk(paste(script.folder, "helper_functions.R", sep = ""))
-read_chunk(paste(script.folder, "load_files.R", sep = ""))
-read_chunk(paste(script.folder, "load_environment.R", sep = ""))
-
-# ----- install-pgks? ----
-install.pkgs <- F
-
-# ----- init_global ----
+# ----- pui-init_global ----
 source(paste(script.folder, "load_global.R", sep = ""))
 
-# ----- source-analyses
+# ----- pui-source-environment ----
 source(paste(script.folder, "load_environment.R", sep =""))
 read_chunk(paste(script.folder,"params/", param, sep = ""))
