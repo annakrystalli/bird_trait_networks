a29eea3596a203be6c58b1a55a3e9d2d4d561853
annakrystalli <annakrystalli@googlemail.com>
2017-01-04 19:49:50 +0100

    add data.folder to file.setup

--- file_setup.R
+++ file_setup.R
@@ -2,6 +2,7 @@
 ## ---- fs-data-setup ----
 output.folder <- "/Users/Anna/Google Drive/bird trait networks/outputs/"
 input.folder <- "/Users/Anna/Google Drive/bird trait networks/inputs/data/"
+data.folder <- "/Users/Anna/Google Drive/bird trait networks/"
 
 ## ---- fs-script-setup ----
 script.folder <- "~/Documents/workflows/bird_trait_networks/"
