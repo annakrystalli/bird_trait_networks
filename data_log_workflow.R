1947d6284fe3b994179a58de2f7f671768814613
annakrystalli <annakrystalli@googlemail.com>
2017-01-11 11:57:23 +0000

    first commit

---
+++ data_log_workflow.R
@@ -0,0 +1,11 @@
+
+setwd(script.folder)
+
+create_data_log(fcodes, file.names = c("Table.csv", "Amniote_Database_Aug_2015.csv"), 
+                overwrite = F) 
+data_log <- update_data_log(overwrite = F)
+
+vnames <- create_vnames(overwrite = F)
+vnames <- update_vnames(overwrite = T)
+
+source('shiny/data_log.R')
