fc8d66557737d0bda9e2d3010caa471670beb0af
annakrystalli <annakrystalli@googlemail.com>
2017-01-11 11:44:21 +0000

    Standardise read.csv function

--- load_files.R
+++ load_files.R
@@ -1,17 +1,16 @@
-#' ---
-#' output:
-#' html_document:
-#' self_contained: false
-#' ---
-#' 
-#' ### load metadata
-# ---- require-dplyr ----
+# ---- load-sys.ref ----
 metadata <- read.csv(paste(input.folder, "metadata/","metadata.csv", sep = ""), 
-                     stringsAsFactors = F, fileEncoding = "mac")
-metadata <- data.frame(apply(metadata, MARGIN = 2, FUN = trimws), stringsAsFactors = F)
+                     stringsAsFactors = F, fileEncoding = fileEncoding, 
+                     na.strings=na.strings, strip.white = T, 
+                     blank.lines.skip = T)
+
+data_log <- read.csv(file = paste(input.folder, "metadata/data_log.csv", sep = ""),
+                     stringsAsFactors = F, fileEncoding = fileEncoding, 
+                     na.strings=na.strings, strip.white = T, 
+                     blank.lines.skip = T)
 
 vnames <- read.csv(paste(input.folder, "metadata/","vnames.csv", sep = ""), 
-                   stringsAsFactors = F, fileEncoding = "mac") 
-vnames <- data.frame(apply(vnames, 2, FUN = trimws), stringsAsFactors = F)  
-vnames[vnames == ""] <- NA
+                   stringsAsFactors = F, fileEncoding = fileEncoding,
+                   na.strings=na.strings, strip.white = T, 
+                   blank.lines.skip = T, header = T)
 
