267fe7ad97c496f525c3fca223ad17f496fd3a4e
annakrystalli <annakrystalli@googlemail.com>
2017-01-11 11:45:00 +0000

    remove read.chunk code

--- project_ui.R
+++ project_ui.R
@@ -4,4 +4,4 @@ source(paste(script.folder, "load_global.R", sep = ""))
 
 # ----- pui-source-environment ----
 source(paste(script.folder, "load_environment.R", sep =""))
-read_chunk(paste(script.folder,"params/", param, sep = ""))
+
