5e3a8fdcd6797e1d4aa1bd13d7b2c53c52a8a09d
annakrystalli <annakrystalli@googlemail.com>
2017-01-11 11:44:00 +0000

    setup for missing wkf and param

--- load_environment.R
+++ load_environment.R
@@ -1,43 +1,36 @@
-#' ---
-#' output:
-#' html_document:
-#' self_contained: false
-#' ---
-
 ## ---- source-params ----
-source(paste(script.folder, "params/", param, sep = ""))
-
-## ---- load_rmacro ----
-if(wkf == "rmacro"){
-  
+if(exists("param")){
+  source(paste(script.folder, "params/", param, sep = ""))
 }
-
-## ---- load_phylocor ----
-if(wkf == "phylocor"){
-  source(paste(script.folder, "PhyloCor functions.R", sep = ""))
+## ---- source-wkf ----
+## load_rmacro
+if(exists("wkf")){ 
+  if(wkf == "rmacro"){
+    eval(parse(text = getURL("https://raw.githubusercontent.com/annakrystalli/rmacroRDM/master/R/functions.R", ssl.verifypeer = FALSE)))
+    eval(parse(text = getURL("https://raw.githubusercontent.com/annakrystalli/rmacroRDM/master/R/wideData_function.R", ssl.verifypeer = FALSE)))
+  }
+  ## load_phylocor
+  if(wkf == "phylocor"){
+    source(paste(script.folder, "PhyloCor functions.R", sep = ""))
+  }
+  ## load_phylonetworker
+  if(wkf == "phylonetworker"){
+    source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
+    source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
+  }
+  # load_mcmc
+  if(wkf == "mcmc"){
+    source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
+    source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
+  }
+  ## load_mgm
+  if(wkf == "mgm"){
+    source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
+    source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
+  }
+  ## load_gk
+  if(wkf == "goodmankruskal"){
+    source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
+    source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
+  }
 }
-
-## ---- load_phylonetworker ----
-if(wkf == "phylonetworker"){
-  source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
-  source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
-}
-
-## ---- load_mcmc ----
-if(wkf == "mcmc"){
-  source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
-  source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
-}
-
-## ---- load_mgm ----
-if(wkf == "mgm"){
-  source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
-  source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
-}
-
-## ---- load_mgm ----
-if(wkf == "goodmankruskal"){
-  source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
-  source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
-}
-
