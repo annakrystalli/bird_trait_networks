bc7c7e00cbe2697d145aeeefe61b178a5840f3ae
annakrystalli <annakrystalli@googlemail.com>
2017-01-11 16:31:29 +0000

    strip to 4 wkf. Add exists("param") to load wkf

--- load_environment.R
+++ load_environment.R
@@ -4,22 +4,13 @@ if(exists("param")){
 }
 ## ---- source-wkf ----
 ## load_rmacro
-if(exists("wkf")){ 
+if(exists("wkf") & exists("param")){ 
   if(wkf == "rmacro"){
     eval(parse(text = getURL("https://raw.githubusercontent.com/annakrystalli/rmacroRDM/master/R/functions.R", ssl.verifypeer = FALSE)))
     eval(parse(text = getURL("https://raw.githubusercontent.com/annakrystalli/rmacroRDM/master/R/wideData_function.R", ssl.verifypeer = FALSE)))
   }
-  ## load_phylocor
-  if(wkf == "phylocor"){
-    source(paste(script.folder, "PhyloCor functions.R", sep = ""))
-  }
   ## load_phylonetworker
   if(wkf == "phylonetworker"){
-    source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
-    source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
-  }
-  # load_mcmc
-  if(wkf == "mcmc"){
     source(paste(script.folder, "PhyloNetworker_functions.R", sep = ""))
     source(paste(script.folder, "PhyloNetworker_setup.R", sep = ""))
   }
