753bd7460c4dc7e8b990abf48cd1f5e420a1a28b
annakrystalli <annakrystalli@googlemail.com>
2017-01-11 11:45:21 +0000

    add render script

---
+++ render.R
@@ -0,0 +1,47 @@
+rm(list = ls())
+
+file_setup_path <- "~/Documents/workflows/bird_trait_networks/file_setup.R"
+source(file_setup_path)
+require(rmarkdown)
+
+file <- "mgm_viz.Rmd"
+render(paste("tools/", file, sep =""), 
+       output_dir = paste(output.folder, "Reports/Results/", sep = ""))
+
+file <- "data_gap_eval.Rmd"
+render(paste("reports/results/", file, sep =""), output_format = "html_notebook",
+output_dir = paste(output.folder, "Reports/Results/", sep = ""))
+
+
+
+
+file <- "GoodmanKruskal.Rmd"
+render(paste("reports/workflow_documentation/", file, sep =""), output_format = "html_notebook",
+output_dir = paste(output.folder, "Reports/workflow documentation/", sep = ""))
+
+file <- "Categorical_phylocors.Rmd"
+render(paste("reports/results/", file, sep =""), output_format = "html_notebook",
+       output_dir = paste(output.folder, "Reports/Results/", sep = ""))
+
+source(paste0(script.folder, "params/phylonetworker.R"))
+file <- "Network_viz.Rmd"
+render(paste("reports/results/", file, sep =""), output_format = "html_notebook",
+       output_file = paste(output.folder,"reports/results/",gsub(".Rmd", paste0("_",an.ID), 
+                                                         file), ".nb.html", sep =""))
+
+
+# render site
+# 
+files <- c("PhyloNetworker_README.Rmd", "pglsPhyloCor_README.Rmd")
+for(file in files){
+render(paste0("man/", file), output_format = "html_notebook",
+       output_file = paste0(script.folder,"docs/",gsub(".Rmd", ".nb.html", file)))}
+
+file <- "project_README.Rmd"
+  render(paste0("man/", file), output_format = "html_notebook",
+         output_file = paste0(script.folder,"docs/",gsub(".Rmd", ".nb.html", file)))
+
+  file <- "project_ui.Rmd"
+  render(paste0("man/", file), output_format = "html_notebook",
+         output_file = paste0(script.folder,"docs/",gsub(".Rmd", ".nb.html", file)))
+  
