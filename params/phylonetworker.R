16ed983ad8dc4779bd4c5e05e765ea5c02c0c15c
annakrystalli <annakrystalli@googlemail.com>
2017-01-11 11:47:43 +0000

    update params

--- params/phylonetworker.R
+++ params/phylonetworker.R
@@ -1,10 +1,10 @@
-# ---- params ----
+# ---- pm-params ----
 #an.ID <- "100spp"
 an.ID <- "all"
 min.n <- 50
 log <- T
 cutoff <- 0.35
 min.cat <- 5
-log <- T
 remove_dtips <- T
 dist.types <- "all"
+save <- T
---
+++ params/project_ui.R
@@ -0,0 +1,3 @@
+## ---- project_ui-params ----
+na.strings <- c("","NA", " ", "-999") 
+fileEncoding <- "mac"
