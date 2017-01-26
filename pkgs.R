e1cfd07ed09ee61906b1ba1a67394fa02e7b7ccc
annakrystalli <annakrystalli@googlemail.com>
2017-01-26 12:20:31 +0000

    update packages

--- pkgs.R
+++ pkgs.R
@@ -1,6 +1,6 @@
 ## ---- pkgs ----
-pkgs <- c("dplyr", "taxize","caper", "geiger","PHYLOGR", 
-"rnetcarto", "igraph", "phytools", "DT", "vegan",
+pkgs <- c( "munsell", "ggplot2","gtable", "dplyr", "taxize","caper", "geiger","PHYLOGR", 
+"rnetcarto", "igraph", "phytools", "DT", "vegan", "RColorBrewer",
 "MCMCglmm", "mgm", "devtools", "flexdashboard", "knitr", 
 "rmarkdown", "markdown", "shiny", "missForest", "networkD3", "Rphylopars",
 "GoodmanKruskal", "plotly", "reshape2", "RCurl", "plotrix", "visNetwork", "tibble")
