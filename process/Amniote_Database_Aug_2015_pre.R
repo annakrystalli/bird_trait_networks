813620159ffc6b70acb6be5a61b6918490fcbd19
annakrystalli <annakrystalli@googlemail.com>
2017-05-27 16:10:45 +0100

    harmonise repro.age.diff calculation with other .ssd variables

--- process/Amniote_Database_Aug_2015_pre.R
+++ process/Amniote_Database_Aug_2015_pre.R
@@ -5,7 +5,7 @@ data <- data[data$class == "Aves",]
 # correct typo in "male_maturity_d"
 data[paste(data$genus, "_", data$species, sep = "") == "Rhea_americana","male_maturity_d"] <- data[paste(data$genus, "_", data$species, sep = "") == "Rhea_americana","male_maturity_d"]/10
 
-add_keep.dat <- data.frame(repro.age.diff = data$female_maturity_d - data$male_maturity_d)
+add_keep.dat <- data.frame(repro.age.diff = log(data$male_maturity_d/data$female_maturity_d))
 add_keep.vars <- "repro.age.diff"
 
 
